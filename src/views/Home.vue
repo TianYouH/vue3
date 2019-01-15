5d60a43f6eadc7770466ca73c2a45c428b470fdc
huangjinliang <huangjinliang@zto.cn>
2019-01-15 21:03:20 +0800

    初始化 vue 模版库

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <video src="http://10.10.133.10:9090/hls/test.m3u8"></video>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
