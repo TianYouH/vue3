59d883dd7d7e8378782d0c7f25b75ca8b851a629
huangjinliang <huangjinliang@zto.cn>
2019-03-04 20:36:02 +0800

    临时

---
+++ src/views/Video.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="video">
+    <iframe src="https://www.baidu.com" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default class Video extends Vue {}
+</script>
+<style lang="scss" scoped></style>
